3fcb0eec10a083a3d15c59d23bd2230fd57e7526
Isac <isac4322@gmail.com>
2021-03-18 19:42:45 -0300

    Tratanda erros com excecao personalizada

--- JogoDeXadrez/tabuleiro/Tabuleiro.cs
+++ JogoDeXadrez/tabuleiro/Tabuleiro.cs
@@ -17,11 +17,42 @@
         {
             return pecas[linha, coluna];
         }
+        public Peca Peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool ExistePeca(Posicao pos) 
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
 
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nesta posição");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos) 
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >=colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos) 
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
         }
     }
 }
